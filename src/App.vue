b3812340925de15bde30b9755bb878ea6453cad7
Strittmatter, Stephan <Stephan.Strittmatter@sybit.de>
2025-10-20 09:13:00 +0200

    chore: Footer added

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import FooterBar from './components/FooterBar.vue'
 import TopNavigation from './components/TopNavigation.vue'
 </script>
 
@@ -6,7 +7,10 @@ import TopNavigation from './components/TopNavigation.vue'
   <header>
     <TopNavigation class="fixed top-0 left-0 z-500 flex w-full" />
   </header>
-  <main class="container mx-auto px-4 py-8 mt-10">
+  <main class="container mx-auto px-4 py-8 mt-16">
     <RouterView />
   </main>
+  <footer class="bg-gray-100 py-4 mt-8">
+    <FooterBar class="container mx-auto" />
+  </footer>
 </template>
---
+++ src/components/FooterBar.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="my-4">
+    <p class="text-gray-600">&copy; 2025 My Vue App. All rights reserved.</p>
+  </div>
+</template>
