cca3e2a3c52e446e182e5760083977175d1a56a8
Strittmatter, Stephan <Stephan.Strittmatter@sybit.de>
2025-10-20 09:06:17 +0200

    Fix: Tailwind & TopMenu

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,12 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-import TheNavigation from './components/TheNavigation.vue'
+import TopNavigation from './components/TopNavigation.vue'
 </script>
 
 <template>
-  <TheNavigation />
-  
-  <div class="container mx-auto px-4 py-8">
-    <header class="leading-normal max-h-screen mt-4">
-      <img alt="Vue logo" class="block mx-auto mb-8 md:mx-0 md:mr-8" src="@/assets/logo.svg" width="125" height="125" />
-
-      <div class="md:flex md:items-start md:flex-wrap">
-        <HelloWorld msg="You did it!" />
-      </div>
-    </header>
-
-    <main class="mt-8">
-      <RouterView />
-    </main>
-  </div>
+  <header>
+    <TopNavigation class="fixed top-0 left-0 z-500 flex w-full" />
+  </header>
+  <main class="container mx-auto px-4 py-8 mt-10">
+    <RouterView />
+  </main>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,7 @@
+import tailwindcss from '@tailwindcss/vite'
+import flowbite from 'flowbite/plugin'
+import forms from '@tailwindcss/forms'
+
 module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
@@ -7,5 +11,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [tailwindcss(), flowbite(), forms()],
 }
