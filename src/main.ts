5ca8e7a5d06145e8b52c286608372e70aef50823
Strittmatter, Stephan <Stephan.Strittmatter@sybit.de>
2025-10-21 10:04:57 +0200

    remove general import

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import './assets/main.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import * as FlowbiteVue from 'flowbite-vue'
 
 import App from './App.vue'
 import router from './router'
@@ -11,12 +10,5 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
-// Registriere alle Flowbite-vue Komponenten global
-Object.entries(FlowbiteVue).forEach(([name, component]) => {
-  if (name !== 'default' && typeof component === 'object') {
-    app.component(name, component)
-  }
-})
 
 app.mount('#app')
