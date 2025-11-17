646834c7b66552200dd1a1787652de916d0c1b5a
FFHDAV <david_woerner@web.de>
2025-11-17 14:28:51 +0000

    AboutUs

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,42 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Das sind wir, unsere Stadt: Radolfzell!</h1>
+    <br>
+
+    <p>Wir sind eine Stadt die sich für die Umwelt einsetzt und eigene Verantwortung in die Hand nimmt!
+      Die Schlussfolgerung ist, dass wir einen echten Mehrwert schaffen wollen und können.
+      <br><br>
+    </p>
+
+  <ul style="list-style-type:square;">
+
+    <p>Die Themen lauten wie folgt:</p>
+      <li>Zusammenarbeit stärkt Zufriedenheit</li>
+      <li>Gestaltung der Zukunft</li>
+      <li>Verantwortung</li>
+
+  </ul>
+<br>
+
+<p>Das gemeinsame Handeln bewegt einen großen Fortschritt in die Richtung unserer Gesellschaft und unserer Umwelt</p>
+
   </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
   .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
+
   }
+  body{
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 20px;
+
+  }
+  h1{
+    font-size: 30px;
+  }
+
 }
 </style>
+
